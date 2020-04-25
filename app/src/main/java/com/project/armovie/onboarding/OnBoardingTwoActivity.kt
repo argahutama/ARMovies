6ddfedc4cc049a45b51d83fba7e3ffc9c1e3b3c7
argahutama <argahut@gmail.com>
2020-04-26 02:10:34 +0700

    first commit

---
+++ app/src/main/java/com/project/armovie/onboarding/OnBoardingTwoActivity.kt
@@ -0,0 +1,13 @@
+package com.project.armovie.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.armovie.R
+
+class OnBoardingTwoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_two)
+    }
+}
