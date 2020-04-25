9fc8599d6a817e5e1fd5113acfbf569425813623
argahutama <argahut@gmail.com>
2020-04-26 04:53:12 +0700

    3rd commit

--- app/src/main/java/com/project/armovie/CheckoutFailedActivity.kt
+++ app/src/main/java/com/project/armovie/CheckoutFailedActivity.kt
@@ -3,10 +3,10 @@ package com.project.armovie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CheckoutActivity : AppCompatActivity() {
+class CheckoutFailedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_checkout)
+        setContentView(R.layout.activity_checkout_failed)
     }
 }
--- app/src/main/java/com/project/armovie/EditProfileActivity.kt
+++ app/src/main/java/com/project/armovie/EditProfileActivity.kt
@@ -3,10 +3,10 @@ package com.project.armovie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SignUpActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_edit_profile)
     }
 }
