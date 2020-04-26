ea04efa1c15229d8e073412f400b36ab218378b3
argahutama <argahut@gmail.com>
2020-04-26 07:56:24 +0700

    4th commit

--- app/src/main/java/com/project/armovie/SplashScreenActivity.kt
+++ app/src/main/java/com/project/armovie/SplashScreenActivity.kt
@@ -8,5 +8,14 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        var handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreenActivity,
+                OnboardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
     }
+}
 }
---
+++ app/src/main/java/com/project/armovie/sign/User.kt
@@ -0,0 +1,4 @@
+package com.project.armovie.sign
+
+class User {
+}
